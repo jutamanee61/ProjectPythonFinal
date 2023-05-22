15b9ed70e23d22f0b9d6d2e51a481fb227d947c6
jutamanee61 <52960791+jutamanee61@users.noreply.github.com>
2023-05-22 10:55:29 +0700

    import PyQt5 library and forex

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello Welcome")
+
+import sys
+
+from PyQt5.QtWidgets import *
+from forex_python.converter import CurrencyRates
+
+class Cafe(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupui_cafe()
+#----------------------------Title-----------------------------------
+    def setupui_cafe(self):
+        self.resize(1350, 800)
+        self.move(60, 80)
+        self.setWindowTitle("Coffee Shop")
+# ----------------------------Payment-----------------------------------
+        c = CurrencyRates()
+        c.get_rates('USD')
+        print(c.get_rates('USD'))
+    #----------------------------END-------------------------------------
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    form = Cafe()
+    form.show()
+    app.exec()
